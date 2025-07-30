034f6bcdaa7fd1ddc4cc0f5addbbe16019dcc975
Nath-FCMB <162546253+Nath-FCMB@users.noreply.github.com>
2025-07-30 10:21:19 +0100

    Flex wallet modification

--- src/main/java/com/techservices/digitalbanking/DigitalBankingApplication.java
+++ src/main/java/com/techservices/digitalbanking/DigitalBankingApplication.java
@@ -41,35 +41,5 @@ public class DigitalBankingApplication {
 
     @PostConstruct
     public void init() {
-        for (Customer foundCustomer : customerRepository.findAll()) {
-            if (foundCustomer.getExternalId() != null) {
-                try {
-                    Long clientId = Long.parseLong(foundCustomer.getExternalId());
-                    @Valid Set<@Valid GetClientsSavingsAccounts> clientAccounts = clientService.getClientAccountsByClientId(String.valueOf(clientId), "savingsAccounts").getSavingsAccounts();
-
-                    Optional<@Valid GetClientsSavingsAccounts> recurringAccount = clientAccounts.stream().filter(account -> account != null && account.isAccountType(AccountType.RECURRING_DEPOSIT)).findAny();
-                    if (recurringAccount.isEmpty() || !recurringAccount.get().getStatus().getActive() || !Objects.equals(recurringAccount.get().getProductId(), 12L)) {
-                        PostRecurringDepositAccountsRequest request = new PostRecurringDepositAccountsRequest();
-                        request.setProductId(12L);
-                        request.setClientId(String.valueOf(clientId));
-                        request.setDepositPeriodFrequencyId(0L);
-                        request.setDepositPeriod(1L);
-                        request.setMandatoryRecommendedDepositAmount(BigDecimal.ONE);
-                        request.setRecurringFrequency(1L);
-                        request.setRecurringFrequencyType(0L);
-                        PostRecurringDepositAccountsResponse response = recurringDepositAccountService.submitApplication(request, true);
-                        if (response != null) {
-                            System.err.println("Recurring deposit account created successfully: {}" + response);
-                            foundCustomer.setRecurringDepositAccountId(String.valueOf(response.getSavingsId()));
-                            customerRepository.save(foundCustomer);
-                        } else {
-                            System.err.println("Failed to create recurring deposit account for client ID: {}");
-                        }
-                    }
-                } catch (Exception e) {
-                    System.err.println("Error processing customer with external ID: " + foundCustomer.getExternalId() + ", " + e.getMessage());
-                }
-            }
-        }
     }
 }
